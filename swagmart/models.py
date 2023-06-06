930038f27cefebf32fe969ae85f4354e8ca69ba1
Muhammad Awais <awaistopper20@gmail.com>
2023-06-06 20:48:12 +0500

    Added models.

--- swagmart/models.py
+++ swagmart/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    AUDIENCE_CHOICES = [
+    ('men', 'Men'),
+    ('women', 'Women')
+    ]
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    target_audience = models.CharField(max_length=255, choices=AUDIENCE_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+class Collection(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE, related_name='products')
+
+    def __str__(self):
+        return self.name
+    
+class Promotions(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    discount = models.FloatField()
+    products = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return self.description
